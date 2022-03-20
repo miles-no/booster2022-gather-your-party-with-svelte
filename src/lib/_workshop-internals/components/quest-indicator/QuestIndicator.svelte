8d03652b9038f101f7cc94de7d54c109efa1e29e
Alexander Castillo <p.alexcast@gmail.com>
2022-03-20 13:44:23 +0100

    Remove default observe options

--- src/lib/_workshop-internals/components/quest-indicator/QuestIndicator.svelte
+++ src/lib/_workshop-internals/components/quest-indicator/QuestIndicator.svelte
@@ -1,45 +1,43 @@
 <script lang="ts">
-	import { onDestroy, onMount } from 'svelte';
-	import { currentQuest } from '$lib/_workshop-internals/stores/quest-log-store';
+    import {onDestroy, onMount} from 'svelte';
+    import {currentQuest} from '$lib/_workshop-internals/stores/quest-log-store';
 
-	const currentQuestMarker = 'data-quest-current';
-	const currentQuestClass = 'active-quest-indicator';
+    const currentQuestMarker = 'data-quest-current';
+    const currentQuestClass = 'active-quest-indicator';
 
-	let observer: MutationObserver;
-	let questToDisplay: string = undefined;
+    let observer: MutationObserver;
+    let questToDisplay: string = undefined;
 
-	const renderQuestIndicators = (): void => {
-		const activeIndicators = document.body.querySelectorAll(`[${currentQuestMarker}]`);
-		activeIndicators?.forEach((node) => {
-			node.removeAttribute(currentQuestMarker);
-			node.classList.remove(currentQuestClass);
-		});
+    const renderQuestIndicators = (): void => {
+        const activeIndicators = document.body.querySelectorAll(`[${currentQuestMarker}]`);
+        activeIndicators?.forEach((node) => {
+            node.removeAttribute(currentQuestMarker);
+            node.classList.remove(currentQuestClass);
+        });
 
-		const questNodes = document.body.querySelectorAll(`[data-quest-${questToDisplay}]`);
-		questNodes?.forEach((node) => {
-			node.setAttribute(currentQuestMarker, '');
-			node.classList.add(currentQuestClass);
-		});
-	};
+        const questNodes = document.body.querySelectorAll(`[data-quest-${questToDisplay}]`);
+        questNodes?.forEach((node) => {
+            node.setAttribute(currentQuestMarker, '');
+            node.classList.add(currentQuestClass);
+        });
+    };
 
-	onMount(() => {
-		observer = new MutationObserver(() => {
-			renderQuestIndicators();
-		});
-		observer.observe(document.body, {
-			childList: true,
-			subtree: true,
-			attributes: false,
-			characterData: false,
-		});
+    onMount(() => {
+        observer = new MutationObserver(() => {
+            renderQuestIndicators();
+        });
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true,
+        });
 
-		currentQuest.subscribe((current) => {
-			questToDisplay = current;
-			renderQuestIndicators();
-		});
-	});
+        currentQuest.subscribe((current) => {
+            questToDisplay = current;
+            renderQuestIndicators();
+        });
+    });
 
-	onDestroy(() => {
-		observer?.disconnect();
-	});
+    onDestroy(() => {
+        observer?.disconnect();
+    });
 </script>
